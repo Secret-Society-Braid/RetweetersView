67381fa7a8fd5ad457cb6eca9ad341bc53a00b9d
hizumiaoba-tablet <hizumiaoba@gmail.com>
2022-08-06 11:34:12 +0900

    add test

---
+++ src/test/java/org/braid/society/secret/retweetersview/EnvironmentTest.java
@@ -0,0 +1,13 @@
+package org.braid.society.secret.retweetersview;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class EnvironmentTest {
+
+    @Test
+    void envTest() {
+        assertTrue(true);
+    }
+}

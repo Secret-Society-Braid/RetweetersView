9fcaa1d2ab0a47a2dfb1b9a9c2dcb74f5685564a
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-08-08 21:31:19 +0900

    test: add tests for twitter operation

---
+++ src/test/java/org/braid/society/secret/retweetersview/twitter/TwitterOperationTest.java
@@ -0,0 +1,21 @@
+package org.braid.society.secret.retweetersview.twitter;
+
+import org.braid.society.secret.retweetersview.lib.twitter.TwitterOperation;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TwitterOperationTest {
+
+    TwitterOperation operation;
+
+    @BeforeAll
+    static void setUp() {
+
+    }
+
+    @Test
+    void credentialsAreValid() {
+
+    }
+
+}

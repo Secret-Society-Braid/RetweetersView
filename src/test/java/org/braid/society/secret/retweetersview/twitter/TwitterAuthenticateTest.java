9c06bbc8e48df0cc95f204b05a1556ade6fe85ed
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-09-12 18:03:19 +0900

    feat: complete implementation for authenticate to twitter

---
+++ src/test/java/org/braid/society/secret/retweetersview/twitter/TwitterAuthenticateTest.java
@@ -0,0 +1,60 @@
+package org.braid.society.secret.retweetersview.twitter;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyOrNullString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.pkce.PKCE;
+import com.github.scribejava.core.pkce.PKCECodeChallengeMethod;
+import com.twitter.clientlib.auth.TwitterOAuth20Service;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import javax.swing.JOptionPane;
+import org.braid.society.secret.retweetersview.lib.util.Base64ControllUtil;
+import org.braid.society.secret.retweetersview.lib.util.PropertiesFileController;
+import org.junit.jupiter.api.Test;
+
+class TwitterAuthenticateTest {
+
+  @Test
+  void getAccessTokenMethodTest() {
+    PropertiesFileController controller = new PropertiesFileController(
+        "twitter_consumer_token.properties");
+    controller.loadPropertiesFromResource();
+    Properties tokenProperties = controller.getProperties();
+    final String clientId = tokenProperties.getProperty("clientId");
+    final String clientSecret = Base64ControllUtil.decodeBase64(tokenProperties.getProperty("clientSecret"));
+
+    OAuth2AccessToken accessToken;
+
+    try(TwitterOAuth20Service service = new TwitterOAuth20Service(
+        clientId,
+        clientSecret,
+        "https://hizumiaoba.github.io/Personal-pages/cushions/retweetersview/",
+        "tweet.read tweet.write users.read offline.access"
+    )) {
+      // log.info("Fetching the Authorization URL...");
+
+      final String secretState = "valuestate";
+      PKCE pkce = new PKCE();
+      pkce.setCodeChallenge("challenge");
+      pkce.setCodeChallengeMethod(PKCECodeChallengeMethod.PLAIN);
+      pkce.setCodeVerifier("challenge");
+      String authorizationUrl = service.getAuthorizationUrl(pkce, secretState);
+
+      final String code = JOptionPane.showInputDialog("Please access this url to grant access to the app.", authorizationUrl);
+      accessToken = service.getAccessToken(pkce, code);
+
+      assertThat(accessToken.getAccessToken(), is(not(emptyOrNullString())));
+      assertThat(accessToken.getRefreshToken(), is(not(emptyOrNullString())));
+
+    } catch (IOException | ExecutionException | InterruptedException e) {
+      fail(e); // replace this for production
+    }
+  }
+
+}
